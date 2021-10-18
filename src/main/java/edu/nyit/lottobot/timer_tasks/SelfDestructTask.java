94528aa685254c39a32f113a3343052c2c0a0835
Aaron Ng <xrydahmsrox@gmail.com>
2021-10-18 11:18:48 -0400

    TimerTask for sending an auto deleting message

---
+++ src/main/java/edu/nyit/lottobot/timer_tasks/SelfDestructTask.java
@@ -0,0 +1,35 @@
+package edu.nyit.lottobot.timer_tasks;
+
+import edu.nyit.lottobot.Main;
+import net.dv8tion.jda.api.JDA;
+
+import java.util.TimerTask;
+
+public class SelfDestructTask extends TimerTask {
+    private long time;
+    private final long channelID;
+    private final long messageID;
+    private final JDA jda;
+
+
+    public SelfDestructTask(long time, long channelID, long messageID, JDA jda) {
+        this.time = time;
+        this.channelID = channelID;
+        this.jda = jda;
+        this.messageID = messageID;
+    }
+
+    @Override
+    public void run() {
+        if (time > 0) {
+            System.out.println(time);
+            time--;
+        } else {
+            System.out.println(messageID);
+            jda.getTextChannelById(channelID).retrieveMessageById(messageID).queue(retrieved -> {
+                retrieved.delete().queue();
+                this.cancel();
+            });
+        }
+    }
+}
