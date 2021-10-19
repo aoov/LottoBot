a780fca6f31545063d46877f29f4c86d40ef1bef
Aaron Ng <xrydahmsrox@gmail.com>
2021-10-19 18:46:09 -0400

    Cleanup

--- src/main/java/edu/nyit/lottobot/handlers/MessageHandlers.java
+++ src/main/java/edu/nyit/lottobot/handlers/MessageHandlers.java
@@ -1,15 +1,14 @@
 package edu.nyit.lottobot.handlers;
 
 import edu.nyit.lottobot.Main;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
+import edu.nyit.lottobot.timer_tasks.SelfDestructTask;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.utils.concurrent.Task;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class MessageHandlers extends ListenerAdapter {
 
@@ -23,54 +22,18 @@ public class MessageHandlers extends ListenerAdapter {
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         //Determines if message is in the proper channel.
         if (main.getDataManager().isBotChannel(event.getGuild().getIdLong(), event.getChannel().getIdLong())) {
-            Message msg = event.getMessage();
-            if (!msg.getAuthor().isBot()) {
-                Timer timer = new Timer(true);
-                timer.schedule(new TimerClass(15, msg), 1000,1000 );
-                super.onMessageReceived(event);
-                System.out.println("test");
-                //DM Test
-                if (msg.getContentRaw().equals("!ping")) {
-                    event.getAuthor().openPrivateChannel().queue(privateChannel ->
-                    {
-                        privateChannel.sendMessage("DM Test").queue();
-                    });
-
-                    //Ping pong
-                    MessageChannel channel = event.getChannel();
-                    long time = System.currentTimeMillis();
-                    channel.sendMessage("Pong!") /* => RestAction<Message> */
-                            .queue(response /* => Message */ -> {
-                                response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
-                            });
-                    super.onMessageReceived(event);
-                }
-            }
+            super.onMessageReceived(event);
         }
     }
 
-    class TimerClass extends TimerTask {
-        private long time;
-        Message msg;
-        Message response;
+    public void replySelfDestructMessage(long userID, long channelID, long time, String message) throws InterruptedException {
+        Timer t = new Timer();
+        var wrapper = new Object(){TimerTask task;};
+        main.getJda().getTextChannelById(channelID).sendMessage("<@" + userID + ">" + ": " + message).queue(sentMessage -> {
+            wrapper.task = new SelfDestructTask(time, channelID, sentMessage.getIdLong(), main.getJda());
+            t.schedule(wrapper.task, 0, 1000);
+        });
 
-        TimerClass(long time, Message msg) {
-            this.time = time;
-            this.msg = msg;
-            msg.getChannel().sendMessage("Timer Starting").queue(response -> {
-                this.response = response;
-            });
-        }
-
-        public void run() {
-            System.out.println(time);
-            if (time > 0) {
-                time--;
-                response.editMessageFormat("Time left: " + time).queue();
-            }else{
-                this.cancel();
-            }
-        }
     }
 }
 
--- src/main/java/edu/nyit/lottobot/managers/DataManager.java
+++ src/main/java/edu/nyit/lottobot/managers/DataManager.java
@@ -1,8 +1,59 @@
 package edu.nyit.lottobot.managers;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class DataManager {
+    private final FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
 
     public DataManager() {
+        setupFirebase();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("server/data");
+    }
+
+    public static void setupFirebase() {
+        FileInputStream serviceAccount = null;
+        try {
+            serviceAccount = new FileInputStream("C:/Users/xrydah/IdeaProjects/LottoBot/serviceAccountKey.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        FirebaseOptions.Builder options = null;
+        try {
+            options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://lottobot-ee1b1-default-rtdb.firebaseio.com");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        FirebaseApp.initializeApp(options.build());
+
+        // As an admin, the app has access to read and write all data, regardless of Security Rules
+        DatabaseReference ref = FirebaseDatabase.getInstance()
+                .getReference("restricted_access/secret_document");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Object document = dataSnapshot.getValue();
+                System.out.println(document);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });
     }
 
     /**
@@ -25,7 +76,15 @@ public class DataManager {
         return 4343;
     }
 
+    public FirebaseDatabase getFirebaseDatabase() {
+        return firebaseDatabase;
+    }
 
+    public DatabaseReference getDatabaseReference() {
+        return databaseReference;
+    }
 
-
+    public void setDatabaseReference(DatabaseReference databaseReference) {
+        this.databaseReference = databaseReference;
+    }
 }
