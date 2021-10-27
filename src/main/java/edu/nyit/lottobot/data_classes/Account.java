01e730a95eca7628959e8de485b1c5bb05c09110
Aaron Ng <xrydahmsrox@gmail.com>
2021-10-27 10:53:06 -0400

    Populated account with basic variables and methods

--- src/main/java/edu/nyit/lottobot/data_classes/Account.java
+++ src/main/java/edu/nyit/lottobot/data_classes/Account.java
@@ -1,4 +1,23 @@
 package edu.nyit.lottobot.data_classes;
 
 public class Account {
+    private final long accountID;
+    private long balance;
+
+    public Account(long accountID, long balance) {
+        this.accountID = accountID;
+        this.balance = balance;
+    }
+
+    public long getAccountID() {
+        return accountID;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
 }
