76bb6f74be240fc5de62e5ca807766371c89addc
Aaron Ng <xrydahmsrox@gmail.com>
2021-10-27 10:58:50 -0400

    Added to string to lottery types

--- src/main/java/edu/nyit/lottobot/data_classes/LotteryType.java
+++ src/main/java/edu/nyit/lottobot/data_classes/LotteryType.java
@@ -2,4 +2,19 @@ package edu.nyit.lottobot.data_classes;
 
 public enum LotteryType {
     POWER_BALL, RAFFLE;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case RAFFLE -> {
+                return "Raffle";
+            }
+            case POWER_BALL -> {
+                return "Power Ball";
+            }
+            default -> {
+                return "FIX IN ENUM";
+            }
+        }
+    }
 }
