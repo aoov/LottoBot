05d67fefeadade34ca132882aba5e3b8794d5e48
Aaron Ng <xrydahmsrox@gmail.com>
2021-11-03 10:10:53 -0400

    Finished up database methods

--- src/main/java/edu/nyit/lottobot/managers/DataManager.java
+++ src/main/java/edu/nyit/lottobot/managers/DataManager.java
@@ -4,20 +4,34 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.database.*;
+import edu.nyit.lottobot.Main;
 import edu.nyit.lottobot.data_classes.Account;
-import edu.nyit.lottobot.data_classes.Lottery;
+import edu.nyit.lottobot.data_classes.LotteryType;
+import edu.nyit.lottobot.data_classes.RaffleLottery;
 
 import java.io.FileInputStream;
-
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.CountDownLatch;
 
 public class DataManager {
     private boolean ready;
+    private HashMap<Long, Account> accounts;
+    private HashMap<String, RaffleLottery> raffleLotteries;
     private final FirebaseDatabase firebaseDatabase;
+    private DatabaseReference accountReference;
+    private DatabaseReference raffleLotteryReference;
+    private Main main;
 
-    public DataManager() {
+    public DataManager(Main main) {
         setupFirebase();
+        accounts = new HashMap<>();
+        raffleLotteries = new HashMap<>();
+        this.main = main;
         firebaseDatabase = FirebaseDatabase.getInstance();
+        updateLocalAccounts(); //Load raffles from database
+        updateLocalRaffles(); //Load raffles from database
     }
 
     //Sets up and connects to the firebase database
@@ -47,7 +61,7 @@ public class DataManager {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Object document = dataSnapshot.getValue();
-                System.out.println(document);
+                //System.out.println(document);
             }
 
             @Override
@@ -78,19 +92,126 @@ public class DataManager {
         return 4343;
     }
 
-
     public void saveAccount(Account account) {
         DatabaseReference accounts = firebaseDatabase.getReference("data").child("accounts");
         accounts.child(account.getAccountID() + "").setValueAsync(account);
     }
 
-    public void saveLottery(Lottery lottery) {
-        DatabaseReference lotteries = firebaseDatabase.getReference("data/lotteries");
-        lotteries.push().setValueAsync(lottery);
+    public void saveAllAccounts() {
+        for (Account account : accounts.values()) {
+            saveAccount(account);
+        }
+    }
+
+    public void saveRaffle(RaffleLottery game) {
+        DatabaseReference lotteries = firebaseDatabase.getReference("data/lotteries/raffles");
+        lotteries.child(game.getUniqueKey()).setValueAsync(game);
+    }
+
+    public boolean updateLocalRaffles() {
+        this.raffleLotteryReference = firebaseDatabase.getReference("data/lotteries/raffles");
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        raffleLotteryReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    boolean active = (boolean) dataSnapshot.child("active").getValue();
+                    long botChannelID = (long) dataSnapshot.child("botChannelID").getValue();
+                    long guildID = (long) dataSnapshot.child("guildID").getValue();
+                    long messageID = (long) dataSnapshot.child("messageID").getValue();
+                    long prizePool = (long) dataSnapshot.child("prizePool").getValue();
+                    ArrayList<Long> allowedRoles = (ArrayList<Long>) dataSnapshot.child("allowedRoles").getValue();
+                    HashMap<String, Long> participants = (HashMap<String, Long>) dataSnapshot.child("participants").getValue();
+                    long startedBy = dataSnapshot.child("startedBy").getValue(Long.class);
+                    long timeLeft = dataSnapshot.child("timeLeft").getValue(Long.class);
+                    String uniqueKey = dataSnapshot.child("uniqueKey").getValue(String.class);
+                    long winner = dataSnapshot.child("winner").getValue(Long.class);
+                    RaffleLottery raffleLottery = new RaffleLottery(guildID, botChannelID, startedBy, prizePool, timeLeft, allowedRoles, main);
+                    if (!participants.isEmpty()) {
+                        raffleLottery.setParticipants(participants);
+                    }
+                    raffleLottery.setUniqueKey(uniqueKey);
+                    raffleLottery.setWinner(winner);
+                    raffleLottery.setMessageID(messageID);
+                    raffleLottery.setActive(active);
+                    raffleLotteries.put(raffleLottery.getUniqueKey(), raffleLottery);
+                }
+                countDownLatch.countDown();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                countDownLatch.countDown();
+            }
+        });
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public boolean updateLocalAccounts() {
+        accountReference = firebaseDatabase.getReference("data").child("accounts");
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        accountReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Account temp = dataSnapshot.getValue(Account.class);
+                    if (temp != null && temp.getAccountID() != -1) {
+                        accounts.put(temp.getAccountID(), temp);
+                    }
+                }
+                countDownLatch.countDown();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                countDownLatch.countDown();
+            }
+        });
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public RaffleLottery getRaffleLottery(String raffleID){
+        if(raffleLotteries.containsKey(raffleID)){
+            return raffleLotteries.get(raffleID);
+        }
+        updateLocalRaffles();
+        return raffleLotteries.getOrDefault(raffleID, null);
+    }
+
+    public RaffleLottery getRaffleLottery(Long messageID){
+        for(RaffleLottery raffleLottery : raffleLotteries.values()){
+            if(raffleLottery.getMessageID() == messageID){
+                return raffleLottery;
+            }
+        }
+        updateLocalRaffles();
+        for(RaffleLottery raffleLottery : raffleLotteries.values()){
+            if(raffleLottery.getMessageID() == messageID){
+                return raffleLottery;
+            }
+        }
+        return null;
+    }
+
+    public Account getAccount(long userID) {
+        if (accounts.containsKey(userID)) {
+            return accounts.get(userID);
+        }
+        updateLocalAccounts();
+        return accounts.getOrDefault(userID, null);
     }
 
     //Getter and setters
-
     public FirebaseDatabase getFirebaseDatabase() {
         return firebaseDatabase;
     }
